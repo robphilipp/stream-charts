94e8d1f2d26a0cab78534507d24342b1aa9312e6
Robert Philipp <rob.philipp@gmail.com>
2020-01-26 09:54:55 -0500

    basic working chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chart from "./Chart";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Chart.tsx
@@ -0,0 +1,67 @@
+import React, {useEffect, useRef} from 'react';
+import Dygraph from 'dygraphs';
+
+const Chart: React.FC = () => {
+
+    const chartDivRef = useRef(null);
+
+    const timeRef = useRef<number>(1);
+    const dataRef = useRef<Array<[number, number]>>(fakeData(10, timeRef.current));
+    const chartRef = useRef<Dygraph>();
+
+    useEffect(
+        () => {
+            chartRef.current = new Dygraph(
+                chartDivRef.current || '',
+                lastNPoints(dataRef.current, 200),
+                {
+                    height: 500,
+                    width: 1000,
+                    drawGrid: false,
+                    strokeWidth: 0,
+                    drawPoints: true,
+                    showRoller: true,
+                    valueRange: [0.0, 1.2],
+                    labels: ['Time', 'Random']
+                }
+            );
+            setInterval(
+                () => {
+                    const numPoints = 1;
+                    dataRef.current = dataRef.current.concat(fakeData(numPoints, timeRef.current));
+                    timeRef.current = timeRef.current + numPoints;
+                    chartRef.current?.updateOptions({'file': lastNPoints(dataRef.current, 200)})
+                },
+                50
+            );
+        },
+        []
+    );
+
+    return (
+        <div ref={chartDivRef}/>
+    )
+};
+
+function fakeData(numPoints: number, start: number): Array<[number, number]> {
+    let points: Array<[number, number]> = [];
+    for (let i = 0; i < numPoints; ++i) {
+        points.push([start + 1, Math.random()]);
+    }
+    return points;
+}
+
+function lastNPoints(data: Array<[number, number]>, lastN: number): Array<[number, number]> {
+    if (lastN > 0 && data.length > lastN) {
+        return data.slice(data.length - lastN);
+    }
+    return data;
+}
+
+function toCsv(data: Array<[number, number]>, lastN: number): string {
+    return lastNPoints(data, lastN)
+        .map(point => `${point[0]},${point[1]}\n`)
+        .reduce((a, b) => `${a}${b}`, '');
+}
+
+export default Chart;
