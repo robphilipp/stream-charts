b8ad9ec4374bfa46b0358b92d884b579eb5bf9da
Robert Philipp <rob.philipp@gmail.com>
2020-02-08 10:16:57 -0500

    renamed the number spinner

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 // import DygraphChart from "./DygraphChart";
 // import PlotlyChart from "./PlotlyChart";
-import D3Chart from "./D3Chart";
+import NumberSpinner from "./NumberSpinner";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       {/*<DygraphChart/>*/}
       {/*<PlotlyChart/>*/}
-      <D3Chart data={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}/>
+      <NumberSpinner data={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}/>
     </div>
   );
 };
--- src/NumberSpinner.tsx
+++ src/NumberSpinner.tsx
@@ -6,7 +6,7 @@ interface OwnProps {
     data?: number[];
 }
 
-function D3Chart(props: OwnProps): JSX.Element {
+function NumberSpinner(props: OwnProps): JSX.Element {
 
     const d3ContainerRef = useRef(null);
 
@@ -84,7 +84,6 @@ function D3Chart(props: OwnProps): JSX.Element {
                 setLiveData(dataRef.current);
             }
         },
-        // [props.data, d3ContainerRef.current, liveData]
         [liveData]
     );
 
@@ -98,71 +97,4 @@ function D3Chart(props: OwnProps): JSX.Element {
     );
 }
 
-export default D3Chart;
-
-
-//     // define time scale
-//     var timeScale = d3.scale.linear()
-//         .domain([300, 1700])
-//         .range([300, 1700])
-//         .clamp(true);
-//     // define value scale
-//     var valueScale = d3.scale.linear()
-//         .domain([0, 1])
-//         .range([30, 95]);
-//     // generate initial data
-//     var normal = d3.random.normal(1000, 150);
-//     var currMs = new Date().getTime() - 300000 - 4000;
-//     var data = d3.range(300).map(function(d, i, arr) {
-//         var value = valueScale(Math.random()); // random data
-// //var value = Math.round((d % 60) / 60 * 95); // ramp data
-//         var interval = Math.round(timeScale(normal()));
-//         currMs += interval;
-//         var time = new Date(currMs);
-//         var obj = { interval: interval, value: value, time: time, ts: currMs }
-//         return obj;
-//     })
-//     // create the real time chart
-//     var chart = realTimeChart()
-//         .title("Chart Title")
-//         .yTitle("Y Scale")
-//         .xTitle("X Scale")
-//         .border(true)
-//         .width(600)
-//         .height(290)
-//         .barWidth(1)
-//         .initialData(data);
-//     console.log("Version: ", chart.version);
-//     console.dir("Dir++");
-//     console.trace();
-//     console.warn("warn")
-//     // invoke the chart
-//     var chartDiv = d3.select("#viewDiv").append("div")
-//         .attr("id", "chartDiv")
-//         .call(chart);
-//     // alternative invocation
-//     //chart(chartDiv);
-//     // drive data into the chart roughly every second
-//     // in a normal use case, real time data would arrive through the network or some other mechanism
-//     var d = 0;
-//     function dataGenerator() {
-//         var timeout = Math.round(timeScale(normal()));
-//         setTimeout(function() {
-//             // create new data item
-//             var now = new Date();
-//             var obj = {
-//                 value: valueScale(Math.random()), // random data
-// //value: Math.round((d++ % 60) / 60 * 95), // ramp data
-//                 time: now,
-//                 color: "red",
-//                 ts: now.getTime(),
-//                 interval: timeout
-//             };
-//             // send the datum to the chart
-//             chart.datum(obj);
-//             // do forever
-//             dataGenerator();
-//         }, timeout);
-//     }
-//     // start the data generator
-//     dataGenerator();
+export default NumberSpinner;
