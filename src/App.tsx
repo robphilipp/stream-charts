1e45511a8b7ff9628ed1c6ced303baa6510217a2
Robert Philipp <rob.philipp@gmail.com>
2020-02-08 13:41:27 -0500

    performance test for faux spikes chart with 3000 elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 // import DygraphChart from "./DygraphChart";
 // import PlotlyChart from "./PlotlyChart";
-import NumberSpinner from "./NumberSpinner";
+// import NumberSpinner from "./NumberSpinner";
+import NumberSpinnerDriver from "./NumberSpinner2";
+
+const data = Array.from({length: 3000}, () => Math.floor(Math.random() * 10));
 
 const App: React.FC = () => {
   return (
     <div className="App">
       {/*<DygraphChart/>*/}
       {/*<PlotlyChart/>*/}
-      <NumberSpinner data={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}/>
+      {/*<NumberSpinner data={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}/>*/}
+      <NumberSpinnerDriver data={data}/>
     </div>
   );
 };
