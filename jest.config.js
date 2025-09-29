41af34a60af1d9f709ad6f6f2598d43a14e2a557
Robert Philipp <rob.philipp@gmail.com>
2025-09-29 19:35:09 -0400

    got jest tests working

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-    reporters: ['default'],
-    // presets: [
-    //     ['@babel/preset-env', {targets: {node: 'current'}}],
-    //     '@babel/preset-typescript',
-    // ],
-    // transformIgnorePatterns: [
-    //     "/node_modules/(?!d3|d3-array|internmap|delaunator|robust-predicates)"
-    // ]
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
