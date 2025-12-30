a2cca5086c493a400284953f51b54b6add10ee64
Robert Philipp <rob.philipp@gmail.com>
2025-12-30 14:35:40 -0500

    updated jest config to work with d3

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // appease jest when using d3 (otherwise import * as d3 from 'd3')
+  moduleNameMapper: {
+    "d3": "<rootDir>/node_modules/d3/dist/d3.min.js",
+    "^d3-(.*)$": "<rootDir>/node_modules/d3-$1/dist/d3-$1.min.js"
+  }
 };
